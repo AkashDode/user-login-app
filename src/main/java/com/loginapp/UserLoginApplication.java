541859453e2f70d58a8873966d25656222544eed
Akash Dode <138995839+AkashDode@users.noreply.github.com>
2026-09-16 14:36:52 -0400

    Extend UserLoginApplication with SpringBootServletInitializer

--- src/main/java/com/loginapp/UserLoginApplication.java
+++ src/main/java/com/loginapp/UserLoginApplication.java
@@ -2,9 +2,16 @@ package com.loginapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserLoginApplication {
+public class UserLoginApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserLoginApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserLoginApplication.class, args);
